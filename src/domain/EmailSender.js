74f30708c9c2ee1285fe518733e5393da0ab490d
Daniel Ramos <danielramosacosta1@gmail.com>
2023-09-23 19:40:03 +0200

    fix eslint

--- src/domain/EmailSender.js
+++ src/domain/EmailSender.js
@@ -1,4 +1,5 @@
 export class EmailSender {
+  // eslint-disable-next-line no-unused-vars
   send(email, message) {
     throw new Error("This is an abstract class. You should implement the send method")
   }
--- src/domain/UserRepository.js
+++ src/domain/UserRepository.js
@@ -1,8 +1,10 @@
 export class UserRepository {
+  // eslint-disable-next-line no-unused-vars
   save(user) {
     throw new Error("This is an abstract class. You should implement the save method")
   }
 
+  // eslint-disable-next-line no-unused-vars
   existsByEmail(email) {
     throw new Error("This is an abstract class. You should implement the existsByEmail method")
   }
